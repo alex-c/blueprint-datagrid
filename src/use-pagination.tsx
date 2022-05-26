3e00161c4bd68d6585cfb610b337aab5224c1e06
Alexandre Charoy <alexandre.charoy@gmail.com>
2022-05-26 20:26:42 +0200

    Optimize pager for many pages

--- src/use-pagination.tsx
+++ src/use-pagination.tsx
@@ -17,8 +17,6 @@ export const usePagination = <T extends DataSourceType>(
   const [activePage, setActivePage] = useState<number>(1);
   const numberOfPages = elementsPerPage !== undefined ? calculateNumberOfPages(numberOfElements, elementsPerPage) : 1;
 
-  const getButtonIntent = (pageNumber: number) => (pageNumber === activePage ? Intent.PRIMARY : Intent.NONE);
-
   const renderPaginationControls = () => {
     return (
       <ButtonGroup>
@@ -28,14 +26,30 @@ export const usePagination = <T extends DataSourceType>(
           disabled={activePage === 1}
           onClick={() => setActivePage(activePage - 1)}
         />
-        {[...Array(numberOfPages)].map((_, i) => (
-          <Button
-            key={"button-" + i}
-            text={i + 1}
-            intent={getButtonIntent(i + 1)}
-            onClick={() => setActivePage(i + 1)}
-          />
-        ))}
+        {activePage > 3 ? (
+          <>
+            <Button text={".."} disabled />
+            <Button text={activePage - 2} onClick={() => setActivePage(activePage - 2)} />
+            <Button text={activePage - 1} onClick={() => setActivePage(activePage - 1)} />
+          </>
+        ) : (
+          [...Array(activePage - 1)].map((_, i) => (
+            <Button key={"button-" + i} text={i + 1} onClick={() => setActivePage(i + 1)} />
+          ))
+        )}
+        <Button key={"button-" + activePage} text={activePage} intent={Intent.PRIMARY} />
+        {numberOfPages - activePage >= 3 ? (
+          <>
+            <Button text={activePage + 1} onClick={() => setActivePage(activePage + 1)} />
+            <Button text={activePage + 2} onClick={() => setActivePage(activePage + 2)} />
+            <Button text={".."} disabled />
+          </>
+        ) : (
+          [...Array(numberOfPages - activePage)].map((_, i) => {
+            const index = i + activePage + 1;
+            return <Button key={"button-" + index} text={index} onClick={() => setActivePage(index)} />;
+          })
+        )}
         <Button
           icon={IconNames.ChevronRight}
           disabled={activePage === numberOfPages}
