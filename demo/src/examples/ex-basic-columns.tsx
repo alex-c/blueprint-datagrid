7a3feb90bd97589cd65e53b4fcf1aab41b5fc9a2
Alexandre Charoy <alexandre.charoy@gmail.com>
2022-05-28 14:54:55 +0200

    Add info and examples to basic column demo section

--- demo/src/examples/ex-basic-columns.tsx
+++ demo/src/examples/ex-basic-columns.tsx
@@ -1,12 +1,44 @@
+import { Code } from "@blueprintjs/core";
 import { Column, ColumnType, Datagrid } from "../../../build";
 import { useAppSelector } from "../app/hooks";
 import { Example } from "../components/example";
+
+const createArena = (name: string, address: string) => {
+  return {
+    name,
+    address,
+    toString: () => {
+      return `${name} (${address})`;
+    },
+  };
+};
+
+const data = [
+  {
+    name: "Detroit Red Wings",
+    arena: {
+      name: "Little Ceasars Arena",
+      address: "2645 Woodward Ave, Detroit, MI 48201, United States",
+    },
+  },
+];
+
+const data2 = [
+  {
+    name: "Detroit Red Wings",
+    arena: createArena("Little Ceasars Arena", "2645 Woodward Ave, Detroit, MI 48201, United States"),
+  },
+];
 
 export const ExBasicColumns = () => {
   const { varieties } = useAppSelector(state => state.varieties);
 
   return (
-    <Example id="ex-basic-columns" title="Basic Columns" description="Text, number and boolean columns.">
+    <Example
+      id="ex-basic-columns"
+      title="Basic Columns"
+      description="Supported basic columns arte text, number and boolean columns."
+    >
       <Datagrid dataSource={varieties}>
         <Column field="name" label="Name" />
         <Column field="species" label="Species" />
@@ -14,6 +46,24 @@ export const ExBasicColumns = () => {
         <Column field="shuUpperBound" label="Heat Upper Bound (SHU)" type={ColumnType.NUMBER} />
         <Column field="rare" label="Rare" type={ColumnType.BOOLEAN} />
       </Datagrid>
+      <p className="new-section">
+        If a field contains an object, <Code>toString</Code> is called. The default <Code>toString</Code> will output{" "}
+        <Code>[object Object]</Code>:
+      </p>
+      <Datagrid dataSource={data}>
+        <Column field="name" label="Team Name" />
+        <Column field="arena" label="Arena" />
+      </Datagrid>
+      <p className="new-section">
+        With a custom <Code>toString</Code> method, the output can be customized:
+      </p>
+      <Datagrid dataSource={data2}>
+        <Column field="name" label="Team Name" />
+        <Column field="arena" label="Arena" />
+      </Datagrid>
+      <p className="new-section">
+        For more options for customizting cell rendering, see <a href="#ex-cell-customization">Cell Customization</a>.
+      </p>
     </Example>
   );
 };
