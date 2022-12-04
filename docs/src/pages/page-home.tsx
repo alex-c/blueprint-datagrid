f40cb491ddeee11cce02ab3795e361860abc272b
Alexandre Charoy <alexandre.charoy@gmail.com>
2022-12-04 11:46:10 +0100

    Add code to home page example

--- docs/src/pages/page-home.tsx
+++ docs/src/pages/page-home.tsx
@@ -1,7 +1,8 @@
-import { Intent, Tag } from "@blueprintjs/core";
+import { Callout, Intent, Tag } from "@blueprintjs/core";
 import { IconNames } from "@blueprintjs/icons";
 import { Action, Column, ColumnType, Datagrid, Pager } from "../../../build";
 import { useAppSelector } from "../app/hooks";
+import { Example } from "../components/example";
 import { Page } from "../components/page";
 import { Section } from "../components/section";
 import { Variety } from "../data/pepper-varieties/pepper-varieties";
@@ -10,23 +11,41 @@ const formatSpecies = (s: string) => <Tag>{s.replace("C.", "Capsicum")}</Tag>;
 const editVarietyAction = (_: any, variety: Variety) => alert(`Editing ${variety.name}...`);
 const deleteVarietyAction = (_: any, variety: Variety) => alert(`Deleting ${variety.name}...`);
 
+const combinedExampleCode = `<Datagrid dataSource={varieties}>
+  <Column field="name" label="Name" type={ColumnType.TEXT} filter sortable />
+  <Column field="species" label="Species" type={ColumnType.TEXT} filter sortable formatter={formatSpecies} />
+  <Column field="shuLowerBound" label="Heat Lower Bound (SHU)" type={ColumnType.NUMBER} filter sortable />
+  <Column field="shuUpperBound" label="Heat Upper Bound (SHU)" type={ColumnType.NUMBER} filter sortable />
+  <Column field="rare" label="Rare" type={ColumnType.BOOLEAN} filter sortable />
+  <Action icon={IconNames.EDIT} intent={Intent.PRIMARY} onClick={editVarietyAction} />
+  <Action icon={IconNames.TRASH} intent={Intent.DANGER} onClick={deleteVarietyAction} />
+  <Pager elementsPerPage={5} />
+</Datagrid>`;
+
 export const HomePage = () => {
   const { varieties } = useAppSelector(state => state.varieties);
 
   return (
     <Page title="Documentation" description="Welcome to the Blueprint Datagrid documentation!">
       <Section title="Example">
-        <p>The following quick example shows most of the current features, like paging, sorting and filtering:</p>
-        <Datagrid dataSource={varieties}>
-          <Column field="name" label="Name" type={ColumnType.TEXT} filter sortable />
-          <Column field="species" label="Species" type={ColumnType.TEXT} filter sortable formatter={formatSpecies} />
-          <Column field="shuLowerBound" label="Heat Lower Bound (SHU)" type={ColumnType.NUMBER} filter sortable />
-          <Column field="shuUpperBound" label="Heat Upper Bound (SHU)" type={ColumnType.NUMBER} filter sortable />
-          <Column field="rare" label="Rare" type={ColumnType.BOOLEAN} filter sortable />
-          <Action icon={IconNames.EDIT} intent={Intent.PRIMARY} onClick={editVarietyAction} />
-          <Action icon={IconNames.TRASH} intent={Intent.DANGER} onClick={deleteVarietyAction} />
-          <Pager elementsPerPage={5} />
-        </Datagrid>
+        <p>The following quick example shows most of the current features, like paging, sorting and filtering, row-level actions and custom cell formatting:</p>
+        <Example code={combinedExampleCode}>
+          <Datagrid dataSource={varieties}>
+            <Column field="name" label="Name" type={ColumnType.TEXT} filter sortable />
+            <Column field="species" label="Species" type={ColumnType.TEXT} filter sortable formatter={formatSpecies} />
+            <Column field="shuLowerBound" label="Heat Lower Bound (SHU)" type={ColumnType.NUMBER} filter sortable />
+            <Column field="shuUpperBound" label="Heat Upper Bound (SHU)" type={ColumnType.NUMBER} filter sortable />
+            <Column field="rare" label="Rare" type={ColumnType.BOOLEAN} filter sortable />
+            <Action icon={IconNames.EDIT} intent={Intent.PRIMARY} onClick={editVarietyAction} />
+            <Action icon={IconNames.TRASH} intent={Intent.DANGER} onClick={deleteVarietyAction} />
+            <Pager elementsPerPage={5} />
+          </Datagrid>
+        </Example>
+      </Section>
+      <Section title="Getting Started">
+        <Callout icon={IconNames.WRENCH} intent={Intent.WARNING}>
+          This section is work in progress!
+        </Callout>
       </Section>
     </Page>
   );
